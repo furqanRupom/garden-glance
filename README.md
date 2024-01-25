b00808a96d6baeccc9c4f048751201bb28ce493d
furqanrupom <furqanrupom978@gmail.com>
2024-01-26 01:48:26 +0600

    set up the clinet side

---
+++ README.md
@@ -0,0 +1,1 @@
+## Garden Glance Flower Management dashboard
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className='font-poppins'>
+      <h3 className="text-xl text-center font-bold text-purple-500">GardenGlance client side is pending...</h3>
+    </main>
+  )
+}
+
+export default App
